b1d447839056e057be21f0b56085b5e7f3c8b443
Alejandro Mármol <amarmol20@gmail.com>
2019-12-06 10:34:06 -0600

    Updates

---
+++ precache-manifest.19075bfdeb9dbdb4d24a9935180c89bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79a9e9c0fedec59b4a1dcb8b810d0d3",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6e78c54ca8d4f6deb36d",
+    "url": "/RobotFriends/static/css/main.d6e5a74e.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/RobotFriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "6e78c54ca8d4f6deb36d",
+    "url": "/RobotFriends/static/js/main.0d37fdec.chunk.js"
+  },
+  {
+    "revision": "63e7cea01961d6f62bc3",
+    "url": "/RobotFriends/static/js/runtime-main.5b8ebe3f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
